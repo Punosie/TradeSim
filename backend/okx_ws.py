ee3f4119e1f9fc931a02c1b92875517ea480d808
Shubhankar <shubhankar.kaushik2003@gmail.com>
2025-05-24 16:08:43 +0530

    Initial websocket conn test

---
+++ backend/okx_ws.py
@@ -0,0 +1,22 @@
+import os
+import websockets
+import asyncio
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WS_URL = os.getenv("WS_ENDPOINT")
+PATH = "dataSample/sample.json"
+os.makedirs(os.path.dirname(PATH), exist_ok=True)
+
+async def sub_to_orderbook():
+    async with websockets.connect(WS_URL) as websocket:
+        async for message in websocket:
+            data = json.loads(message)
+            json.dump(data, open(PATH, "w"), indent=4)
+            print("Received data")
+            break
+
+if __name__ == "__main__":
+    asyncio.run(sub_to_orderbook())
