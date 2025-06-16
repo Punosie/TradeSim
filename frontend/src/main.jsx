52cc78293f10d944b75838594ae66177fb0e8fb3
Shubhankar <shubhankar.kaushik2003@gmail.com>
2025-06-16 23:02:15 +0530

    enable vercel speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './router/App.jsx'
-import {AuthProvider} from './context/AuthProvider.jsx'
+import { AuthProvider } from './context/AuthProvider.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <SpeedInsights />
       <App />
     </AuthProvider>
   </StrictMode>,
