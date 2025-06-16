56a913adb10b4d742b2b6da683142b67cda9e15f
Shubhankar <shubhankar.kaushik2003@gmail.com>
2025-06-16 23:39:29 +0530

    chore: bug fixes

--- backend/app/router/routes/trade.py
+++ backend/app/router/routes/trade.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from app.schemas import TradeInput
 from ..handler.trade_handler import TradeHandler
 from app.dependencies.auth import get_current_user
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 import io
 
 router = APIRouter()
@@ -13,6 +13,7 @@ handler = TradeHandler()
 async def simulate_trade(trade_input: TradeInput, _user=Depends(get_current_user)):
     return await handler.simulate_trade(trade_input, _user["uid"])
 
+
 @router.get("/trade-history")
 def get_trade_history(_user=Depends(get_current_user)):
     """
@@ -20,11 +21,10 @@ def get_trade_history(_user=Depends(get_current_user)):
     """
     csv_data = handler.get_trade_history_csv(_user["uid"])
     if not csv_data:
-        return {"message": "No trades yet."}
-    
+        return JSONResponse(content={"message": "No trades yet."}, status_code=404)
+
     return StreamingResponse(
         io.StringIO(csv_data),
         media_type="text/csv",
-        headers={"Content-Disposition": "attachment; filename=trade_history.csv"}
+        headers={"Content-Disposition": "attachment; filename=trade_history.csv"},
     )
-    
