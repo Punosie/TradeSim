a96ea5b29cd7a5ce7b7abfc2c35ba4dd12882f72
Shubhankar <shubhankar.kaushik2003@gmail.com>
2025-06-16 23:55:02 +0530

    chore: fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,9 +33,16 @@ init_firebase()
 
 app = FastAPI(lifespan=lifespan)
 
+origins=[
+    "https://tradesim-shubhabha.vercel.app",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:5173",
+],
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
